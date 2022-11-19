956d19474c46cda7a5d463337a5bf4948b29b3ac
serogaq <36307024+serogaq@users.noreply.github.com>
2022-11-19 19:36:56 +0400

    deleting comment

--- src/Console/GetUpdates.php
+++ src/Console/GetUpdates.php
@@ -235,40 +235,4 @@ class GetUpdates extends Command {
 		$this->complete = true;
 	}
 
-
-	/*public function handle() {
-		$this->username = $this->argument('username');
-		$inBackground = $this->option('background');
-		$isFirstRun = ($this->option('first-run') === 'yes') ? true : false;
-		if(Cache::store('file')->get('tgbotapi:getupdates:forked', '0') === '1' && $isFirstRun) return 0;
-		if($inBackground && $isFirstRun) {
-			Cache::store('file')->put('tgbotapi:getupdates:forked', '1');
-			$this->fork();
-			return 0;
-		}
-		try {
-			$bot = BotManager::selectBot($this->username);
-			$this->bot = $bot;
-		} catch(BotManagerConfigException $e) {
-			$this->error($e->getMessage());
-			return 1;
-		}
-		$end_time = time()+59;
-		while(time() < $end_time) {
-			$t = $end_time-time();
-			if($t < 10 && $inBackground) {
-				$this->fork();
-				return 0;
-			}
-			try {
-				$this->bot->getUpdatesAndCreateEvents(['timeout' => $t]);
-			} catch(\Exception $e) {
-				Cache::store('file')->put('tgbotapi:getupdates:forked', '0');
-				$this->fork(true);
-				return 0;
-			}
-		}
-		$this->fork();
-	}*/
-
 }

d65f3729f2d5d4856754b95f47175c17ed6a64c0
serogaq <36307024+serogaq@users.noreply.github.com>
2022-11-25 01:18:02 +0400

    upd

--- src/Facades/BotManager.php
+++ src/Facades/BotManager.php
@@ -4,7 +4,7 @@ namespace Serogaq\TgBotApi\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class BotManagerFacade extends Facade {
+class BotManager extends Facade {
     protected static function getFacadeAccessor() {
         return Serogaq\TgBotApi\BotManager::class;
     }
